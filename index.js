faeb5c5c303f87301f579b61ad64ec7d125ca105
GoktanBattal <goktanbattal@gmail.com>
2022-08-30 17:22:57 +0300

    index updated

--- index.js
+++ index.js
@@ -180,7 +180,6 @@ function filterByWord(array, string){
 }
 
 
-
 /* 💪💪💪💪💪🧁🍦🍨 STRETCH 🍨🍦🍫💪💪💪💪💪*/ 
 
 /* STRETCH 1: Write a function that returns the average number of words in an array. You should be able to use this function for any array, but can test with originalFlavors.
